69bb8f08e29af54c67ec25fd3141b8e33e117db3
Dhruvin Patel <dhruvinpatel1831@gmail.com>
2021-05-15 21:44:59 +0530

    demo restaurant added

--- src/components/pages/RestaurantSelection.js
+++ src/components/pages/RestaurantSelection.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import "../../styles/RestaurantSelection.css"
+import { Link } from "react-router-dom";
 function RestaurantSelection() {
     return (
         <div>
             Hello These are some best recommendations for you!!
+            <Link to="/restaurant">
+                demo restaurant
+            </Link>
         </div>
     )
 }
